433f209a2ca24be154775e5c2861bbb5bcd682dd
liuchao <408315942@qq.com>
2022-09-08 22:43:54 +0800

    :sparkles: 增加message全局引用

---
+++ src/components/NaiveProvider.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+import { defineComponent, h } from 'vue'
+
+const NaiveProviderContent = defineComponent({
+  name: 'NaiveProviderContent',
+  setup() {
+  },
+  render() {
+    return h('div')
+  },
+})
+</script>
+
+<template>
+  <n-loading-bar-provider>
+    <n-dialog-provider>
+      <n-notification-provider>
+        <n-message-provider>
+          <slot />
+          <NaiveProviderContent />
+        </n-message-provider>
+      </n-notification-provider>
+    </n-dialog-provider>
+  </n-loading-bar-provider>
+</template>
+
+<style scoped>
+
+</style>
